a0a43c3c1417ed13b2914a6511710aad5cde514e
Jose Eduardo <Jose Eduardo@DESKTOP-VIEE161>
2021-12-03 12:49:30 -0700

    Se agrego la suma

--- src/proyecto/lab/Asiento.java
+++ src/proyecto/lab/Asiento.java
@@ -4,6 +4,10 @@ public class Asiento {
     
     public void Holas(){
         System.out.println("Este mensaje es una prubea, Att: Hinojosa");
+        
+        int Suma = 10 + 5;
+        
+        System.out.println("La suma de 10 + 5 = " + Suma);
 }
     
 }
