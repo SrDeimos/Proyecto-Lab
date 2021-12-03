6f6eb0828fb2aa702c05ad9e6a32c42fc245c1e7
Jose Eduardo <Jose Eduardo@DESKTOP-VIEE161>
2021-12-02 18:59:58 -0700

    Update del codigo

---
+++ src/proyecto/lab/Recepcion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto.lab;
+
+/**
+ *
+ * @author Jose Eduardo
+ */
+public class Recepcion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a testing message");
+        System.out.println("Developers: Jose Hinojosa and Paul Vazquez");
+    }
+    
+}
