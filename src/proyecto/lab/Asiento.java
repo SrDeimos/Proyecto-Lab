9b128c88a3ed35d1f543204dec258ade5d7cf6be
paulvazquez <paulvazquez@192.168.0.25>
2021-12-09 16:54:40 -0700

    Clase asiento en progreso

--- src/proyecto/lab/Asiento.java
+++ src/proyecto/lab/Asiento.java
@@ -2,8 +2,43 @@ package proyecto.lab;
 
 public class Asiento {
     
-char cEjecutiva, cEconomica;
-    
-    
-    
+int numeroAsiento;
+    char claseAsiento, ubicacionAsiento;
+    Pasajero pasajero = new Pasajero();
+    public Asiento(){
+        
+    }
+    public Asiento(int numeroAsiento, char claseAsiento, char ubicacionAsiento){
+        
+    }
+    public void asignarNumeroAsiento (int numeroAsiento){
+        this.numeroAsiento = numeroAsiento;
+    }
+    public int obtenerNumeroAsiento (){
+        return numeroAsiento;
+    }
+    public void asignarClaseAsiento(char claseAsiento){
+        this.claseAsiento = claseAsiento;
+    }
+    public char obtenerClaseAsiento(char claseAsiento){
+        return claseAsiento;
+    }
+    public void asignarUbicacionAsiento(char ubicacionAsiento){
+        this.ubicacionAsiento = ubicacionAsiento;
+    }
+    public char obtenerUbicacionAsiento(){
+        return ubicacionAsiento;
+    }
+    public void asignarPasajero(Pasajero pasajero){
+        this.pasajero = pasajero;
+    }
+    public Pasajero obtenerPasajero(){
+        return pasajero;
+    }
+    public void desocuparAsiento(){
+        pasajero = null;
+    }
+    public boolean obtenerEstadoAsiento(){
+        
+    }
 }
