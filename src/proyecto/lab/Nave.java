81cae4135dc6f2bc386e372d7d2db5b0175e4ac7
paulvazquez <paulvazquez@192.168.0.25>
2021-12-09 17:05:03 -0700

    Clase asiento en progreso y nave

--- src/proyecto/lab/Nave.java
+++ src/proyecto/lab/Nave.java
@@ -9,5 +9,41 @@ public class Nave {
         asiento = new Asiento();
 
     }
+    public boolean obtenerEstadoAsientosClaseEjecutiva(){
+        
+    }
+    public boolean obtenerEstadoAsientosClaseEconomica(){
+        
+    }
+    public int obtenerAsientoClaseEjecutivaDesocupado(char ubicacionAsiento){
+        return ubicacionAsiento;
+    }
+    public int obtenerAsientoClaseEconomicaDesocupado(char ubicacionAsiento){
+        return ubicacionAsiento;
+    }
+    public Pasajero ObtenerPasajeroClaseEjecutiva (String nombrePasajero){
+        return nombrePasajero;
+    }
+    public Pasajero obtenerPasajeroClaseEconomica (String nombrePasajero){
+        return nombrePasajero;
+    }
+    public Pasajero obtenerPasajeroClaseEjecutiva(int numeroAsiento){
+        return numeroAsiento;
+    }
+    public Pasajero obtenerPasajeroClaseEconomica(int numeroAsiento){
+        
+    }
+    public void asignarPasajeroClaseEjecutiva(int numeroAsiento, Pasajero){
+        
+    }
+    public void asignarPasajeroClaseEconomica(int numeroAsiento, Pasajero){
+        
+    }
+    public void eliminarPasajeroClaseEjecutiva(int numeroAsiento){
+        
+    }
+    public void eliminarPasajeroClaseEconomica(int numeroAsiento){
+        
+    }
 
 }
