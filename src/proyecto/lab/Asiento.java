a4d7ab109f15ad7079c60658d61e401e3de22654
paulvazquez <paulvazquez@192.168.0.7>
2021-12-10 22:49:22 -0700

    Clase asiento y nave actualizadas

--- src/proyecto/lab/Asiento.java
+++ src/proyecto/lab/Asiento.java
@@ -21,12 +21,45 @@ int numeroAsiento;
     }
     public void asignarClaseAsiento(char claseAsiento){
         this.claseAsiento = claseAsiento;
+        if (numeroAsiento>=1 && numeroAsiento<=4)
+            claseAsiento = 1;
+        else
+            claseAsiento = 2;
     }
     public char obtenerClaseAsiento(char claseAsiento){
         return claseAsiento;
     }
     public void asignarUbicacionAsiento(char ubicacionAsiento){
         this.ubicacionAsiento = ubicacionAsiento;
+        if (claseAsiento==1)
+        {
+            if (numeroAsiento==2||numeroAsiento==3)
+            {
+                ubicacionAsiento=3;
+            }
+            else
+            {
+                if(numeroAsiento==1||numeroAsiento==4)
+                    ubicacionAsiento=1;
+            }
+        }
+        else
+        {
+            if (numeroAsiento==5||numeroAsiento==10||numeroAsiento==11||numeroAsiento==16||numeroAsiento==17||numeroAsiento==22||numeroAsiento==23||numeroAsiento==28)
+            {
+                ubicacionAsiento=1;
+            }
+            else
+            {
+                if(numeroAsiento==6||numeroAsiento==9||numeroAsiento==12||numeroAsiento==15||numeroAsiento==18||numeroAsiento==21||numeroAsiento==24||numeroAsiento==27)
+                {
+                    ubicacionAsiento=2;
+                }
+            }
+            if (numeroAsiento==7||numeroAsiento==8||numeroAsiento==13||numeroAsiento==14||numeroAsiento==19||numeroAsiento==20||numeroAsiento==25||numeroAsiento==26)
+                ubicacionAsiento=3;
+        }
+        //ubicacionAsiento = claseAsiento;
     }
     public char obtenerUbicacionAsiento(){
         return ubicacionAsiento;
--- src/proyecto/lab/Nave.java
+++ src/proyecto/lab/Nave.java
@@ -16,7 +16,28 @@ public class Nave {
         aEjecutivo[3] = new Asiento(4,'1','1');
         aEconomico[0] = new Asiento(5,'2','1');
         aEconomico[1] = new Asiento(6,'2','2');
-        
+        aEconomico[2] = new Asiento(7,'2','3');
+        aEconomico[3] = new Asiento(8,'2','3');
+        aEconomico[4] = new Asiento(9,'2','2');
+        aEconomico[5] = new Asiento(10,'2','1');
+        aEconomico[6] = new Asiento(11,'2','1');
+        aEconomico[7] = new Asiento(12,'2','2');
+        aEconomico[8] = new Asiento(13,'2','3');
+        aEconomico[9] = new Asiento(14,'2','3');
+        aEconomico[10] = new Asiento(15,'2','2');
+        aEconomico[11] = new Asiento(16,'2','1');
+        aEconomico[12] = new Asiento(17,'2','1');
+        aEconomico[13] = new Asiento(18,'2','2');
+        aEconomico[14] = new Asiento(19,'2','3');
+        aEconomico[15] = new Asiento(20,'2','3');
+        aEconomico[16] = new Asiento(21,'2','2');
+        aEconomico[17] = new Asiento(22,'2','1');
+        aEconomico[18] = new Asiento(23,'2','1');
+        aEconomico[19] = new Asiento(24,'2','2');
+        aEconomico[20] = new Asiento(25,'2','3');
+        aEconomico[21] = new Asiento(26,'2','3');
+        aEconomico[22] = new Asiento(27,'2','2');
+        aEconomico[23] = new Asiento(28,'2','1');
         
         pasajero = null;
     }
